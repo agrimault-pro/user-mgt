43399ab623ca466540bc34d85d37216de8637caf
agrimault-pro <42966675+agrimault-pro@users.noreply.github.com>
2018-10-29 21:18:13 +0100

    Introduce Spring cloud security
    There is still work to do to prevent Users to be accessed without Zuul

--- zuul-server/src/main/java/com/usermgt/zuulserver/ZuulServerApplication.java
+++ zuul-server/src/main/java/com/usermgt/zuulserver/ZuulServerApplication.java
@@ -1,17 +1,22 @@
 package com.usermgt.zuulserver;
 
+import com.usermgt.zuulserver.filters.LogFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
+@EnableWebSecurity
 public class ZuulServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulServerApplication.class, args);
 	}
+
 }
