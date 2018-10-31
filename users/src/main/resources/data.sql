d15cb1da15752683332c5edc7791481423d0bdaf
agrimault-pro <42966675+agrimault-pro@users.noreply.github.com>
2018-10-31 11:52:58 +0100

    just some duplicate users to add content

--- users/src/main/resources/data.sql
+++ users/src/main/resources/data.sql
@@ -13,3 +13,35 @@ INSERT INTO USERS (ID , ALIAS ,FIRSTNAME ,LASTNAME ,EMAIL, PHONENUMBER, BIRTHDAT
 
 INSERT INTO USERS (ID , ALIAS ,FIRSTNAME ,LASTNAME ,EMAIL, PHONENUMBER, BIRTHDATE, GENDER, AVATAR, PASSWORD )
     VALUES (5, 'user_5', 'Tom', 'Cruise', 'tom.cruise@gmail.com',' 07 01 55 15 08',to_date('1945/08/06', 'YYYY/MM/DD'),'M','http://www.avatarsdb.com/avatars/kill_bill12.jpg','pwd_5');
+
+
+INSERT INTO USERS (ID , ALIAS ,FIRSTNAME ,LASTNAME ,EMAIL, PHONENUMBER, BIRTHDATE, GENDER, AVATAR, PASSWORD )
+    VALUES (6, 'user_6', 'Jhon', 'Doe', 'jhon.doe@gmail.com',' 06 06 05 05 04',to_date('1975/03/22', 'YYYY/MM/DD'),'M','http://www.avatarsdb.com/avatars/kill_bill_lucy_liu.jpg','pwd_6');
+
+INSERT INTO USERS (ID , ALIAS ,FIRSTNAME ,LASTNAME ,EMAIL, PHONENUMBER, BIRTHDATE, GENDER, AVATAR, PASSWORD )
+    VALUES (7, 'user_7', 'Bruce', 'Lee', 'bruce.lee@gmail.com',' 06 08 05 05 04',to_date('1970/05/15', 'YYYY/MM/DD'),'M','http://www.avatarsdb.com/avatars/the_bride19.jpg','pwd_7');
+
+INSERT INTO USERS (ID , ALIAS ,FIRSTNAME ,LASTNAME ,EMAIL, PHONENUMBER, BIRTHDATE, GENDER, AVATAR, PASSWORD )
+    VALUES (8, 'user_8', 'Brad', 'Pitt', 'brad.pitt@gmail.com',' 07 01 05 05 07',to_date('1969/08/06', 'YYYY/MM/DD'),'M','http://www.avatarsdb.com/avatars/kill_bill12.jpg','pwd_8');
+
+INSERT INTO USERS (ID , ALIAS ,FIRSTNAME ,LASTNAME ,EMAIL, PHONENUMBER, BIRTHDATE, GENDER, AVATAR, PASSWORD )
+    VALUES (9, 'user_9', 'Tom', 'Hanks', 'tom.hanks@gmail.com',' 07 01 05 05 08',to_date('1969/04/06', 'YYYY/MM/DD'),'M','http://www.avatarsdb.com/avatars/kill_bill12.jpg','pwd_9');
+
+INSERT INTO USERS (ID , ALIAS ,FIRSTNAME ,LASTNAME ,EMAIL, PHONENUMBER, BIRTHDATE, GENDER, AVATAR, PASSWORD )
+    VALUES (10, 'user_10', 'Tom', 'Cruise', 'tom.cruise@gmail.com',' 07 01 55 15 08',to_date('1945/08/06', 'YYYY/MM/DD'),'M','http://www.avatarsdb.com/avatars/kill_bill12.jpg','pwd_10');
+
+
+INSERT INTO USERS (ID , ALIAS ,FIRSTNAME ,LASTNAME ,EMAIL, PHONENUMBER, BIRTHDATE, GENDER, AVATAR, PASSWORD )
+    VALUES (11, 'user_11', 'Jhon', 'Doe', 'jhon.doe@gmail.com',' 06 06 05 05 04',to_date('1975/03/22', 'YYYY/MM/DD'),'M','http://www.avatarsdb.com/avatars/kill_bill_lucy_liu.jpg','pwd_11');
+
+INSERT INTO USERS (ID , ALIAS ,FIRSTNAME ,LASTNAME ,EMAIL, PHONENUMBER, BIRTHDATE, GENDER, AVATAR, PASSWORD )
+    VALUES (12, 'user_12', 'Bruce', 'Lee', 'bruce.lee@gmail.com',' 06 08 05 05 04',to_date('1970/05/15', 'YYYY/MM/DD'),'M','http://www.avatarsdb.com/avatars/the_bride19.jpg','pwd_12');
+
+INSERT INTO USERS (ID , ALIAS ,FIRSTNAME ,LASTNAME ,EMAIL, PHONENUMBER, BIRTHDATE, GENDER, AVATAR, PASSWORD )
+    VALUES (13, 'user_13', 'Brad', 'Pitt', 'brad.pitt@gmail.com',' 07 01 05 05 07',to_date('1969/08/06', 'YYYY/MM/DD'),'M','http://www.avatarsdb.com/avatars/kill_bill12.jpg','pwd_13');
+
+INSERT INTO USERS (ID , ALIAS ,FIRSTNAME ,LASTNAME ,EMAIL, PHONENUMBER, BIRTHDATE, GENDER, AVATAR, PASSWORD )
+    VALUES (14, 'user_14', 'Tom', 'Hanks', 'tom.hanks@gmail.com',' 07 01 05 05 08',to_date('1969/04/06', 'YYYY/MM/DD'),'M','http://www.avatarsdb.com/avatars/kill_bill12.jpg','pwd_14');
+
+INSERT INTO USERS (ID , ALIAS ,FIRSTNAME ,LASTNAME ,EMAIL, PHONENUMBER, BIRTHDATE, GENDER, AVATAR, PASSWORD )
+    VALUES (15, 'user_15', 'Tom', 'Cruise', 'tom.cruise@gmail.com',' 07 01 55 15 08',to_date('1945/08/06', 'YYYY/MM/DD'),'M','http://www.avatarsdb.com/avatars/kill_bill12.jpg','pwd_15');
