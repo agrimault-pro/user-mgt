ec92644d4580a9f8422897a02dc810caf747ac7e
agrimault-pro <42966675+agrimault-pro@users.noreply.github.com>
2018-11-23 22:08:52 +0100

    Add a bean to handle CORS (access-control-allow-origin)

--- zuul-server/src/main/java/com/usermgt/zuulserver/ZuulServerApplication.java
+++ zuul-server/src/main/java/com/usermgt/zuulserver/ZuulServerApplication.java
@@ -8,6 +8,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -19,4 +22,22 @@ public class ZuulServerApplication {
 		SpringApplication.run(ZuulServerApplication.class, args);
 	}
 
+
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("HEAD");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("DELETE");
+		config.addAllowedMethod("PATCH");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
 }
