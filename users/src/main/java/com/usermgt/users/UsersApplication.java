f47255dc2d94ae1b37f1d2fc9eb02129b73b8198
agrimault-pro <42966675+agrimault-pro@users.noreply.github.com>
2018-11-26 20:20:25 +0100

    Fix config server

--- users/src/main/java/com/usermgt/users/UsersApplication.java
+++ users/src/main/java/com/usermgt/users/UsersApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigurationProperties
 public class UsersApplication {
 
 	public static void main(String[] args) {
--- zuul-server/src/main/java/com/usermgt/zuulserver/ZuulServerApplication.java
+++ zuul-server/src/main/java/com/usermgt/zuulserver/ZuulServerApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -15,13 +18,13 @@ import org.springframework.web.filter.CorsFilter;
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
+@EnableEurekaClient
 @EnableWebSecurity
 public class ZuulServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulServerApplication.class, args);
 	}
-
 
 	@Bean
 	public CorsFilter corsFilter() {
