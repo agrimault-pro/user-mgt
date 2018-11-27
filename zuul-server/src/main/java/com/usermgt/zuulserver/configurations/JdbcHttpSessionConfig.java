547895698441991a1ea79e5ad9aff66d96d1a412
agrimault-pro <42966675+agrimault-pro@users.noreply.github.com>
2018-11-27 12:39:15 +0100

    New config in Zuul with HttpSession management

---
+++ zuul-server/src/main/java/com/usermgt/zuulserver/configurations/JdbcHttpSessionConfig.java
@@ -0,0 +1,30 @@
+package com.usermgt.zuulserver.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+import org.springframework.transaction.PlatformTransactionManager;
+
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJdbcHttpSession
+public class JdbcHttpSessionConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("org/springframework/session/jdbc/schema-h2.sql")
+                .build();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
